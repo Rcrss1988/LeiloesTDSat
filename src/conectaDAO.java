a968aa0f49ffa019f0f43c5f6bf4433ded0bb00f
“Raphael <rapha.senna@gmail.com>
2023-10-13 17:33:01 -0300

    classe conectaDAO solucionada - sistema conectado ao BD

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,31 +1,26 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
-    public Connection connectDB(){
+    private PreparedStatement st; // Declaração da variável de declaração SQL pré-compilada.
+    private ResultSet rs; // Declaração da variável de resultado de consulta SQL.
+
+    public Connection connectDB() {
+
         Connection conn = null;
-        
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/leiloes_td_sat?useSSL=false";
+            conn = DriverManager.getConnection(url, "root", "472472");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
         }
         return conn;
     }
